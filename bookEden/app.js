7e41ece42c20c09ab0e817461e506e5e1cecdeaa
Lau-code <roblesma.laura@gmail.com>
2020-04-06 00:19:32 -0300

    carrito

--- bookEden/app.js
+++ bookEden/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/bookEden/carrito', compraRouter);
+app.use('/venta-de-libros/bookEden', compraRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
