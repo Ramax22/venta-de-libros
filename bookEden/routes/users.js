1d05e6bfd9905a48c930cdc11ae51e3d19967303
Lau-code <roblesma.laura@gmail.com>
2020-05-05 23:06:49 -0300

    modificacion de registro y login

--- bookEden/routes/users.js
+++ bookEden/routes/users.js
@@ -1,16 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var registerController = require("../controllers/usersControllers");
+var multer = require('multer');
+var path = require('path');
+
+
+var storage = multer.diskStorage({
+	destination:(req,file,cb)=>{
+		cb(null,'public/images/avatars');
+	},
+	filename:(req,file,cb)=>{
+		cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+	}
+});
+var upload = multer({storage:storage});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-router.get('/iniciar', function(req, res, next) {
+router.get('/login', function(req, res, next) {
   res.render('log-in');
 });
-router.get('/registro', function(req, res, next) {
-  res.render('register');
-});
+router.get('/register', registerController.register);
+router.post('/register', upload.any(), registerController.create);
 
 
 module.exports = router;
