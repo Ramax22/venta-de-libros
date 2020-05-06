fed0af02fcc8b212f7ea520f99ed4e9ecec530f3
Miguel Guibarra <miguelguibarra@gmail.com>
2020-05-06 13:59:47 -0300

    product-create funcional

--- bookEden/controllers/productsController.js
+++ bookEden/controllers/productsController.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 var productsController = {
 
     main: function(req, res){
@@ -6,6 +12,39 @@ var productsController = {
         
     create: function(req, res){
         res.render('product-create');
+    },
+    created:function(req,res,next){
+      //funciona para un array vacio o ya empezado
+        var ultimo;
+        
+        if(products.length>0){
+             ultimo=products[products.length-1]
+             ultimo= ultimo.id+1
+        }else{
+             ultimo=1;
+        }
+     
+        
+      
+		let nuevoProducto={
+			id: ultimo,
+			author: req.body.author,
+			price:req.body.price ,
+            discount:req.body.discount ,
+            category:req.body.category,
+			description:req.body.description ,
+            lenguage:req.body.lenguage,
+            format: req.body.format,
+            date:req.body.date,
+            image:req.files[0].filename
+        }
+        console.log(req.files)
+        console.log(nuevoProducto);
+		products.push(nuevoProducto)
+		let modificacion= JSON.stringify(products)
+		fs.writeFileSync(productsFilePath,modificacion)
+
+		res.redirect('/index');
     }
 }
 
