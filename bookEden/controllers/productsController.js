6d5d6663a188586d7a7b985e4c04beb1c781b373
Ramax22 <ramagarbagna@outlook.com>
2020-05-06 16:17:30 -0300

    product detail dinamico con sus funcioens de edit y delete

--- bookEden/controllers/productsController.js
+++ bookEden/controllers/productsController.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 var productsController = {
 
     main: function(req, res){
@@ -6,7 +12,67 @@ var productsController = {
         
     create: function(req, res){
         res.render('product-create');
+    },
+
+    detail : function (req, res) {
+        var selectedProduct = getProduct(req.params.id);
+        res.render('detail', {
+            'selectedProduct' : selectedProduct
+        });
+    },
+
+    edit : function (req, res) {
+        var selectedProduct = getProduct(req.params.id);
+        res.render('product-edit-form', {
+            'selectedProduct' : selectedProduct
+        });
+    },
+
+    update : function (req, res) {
+        for (let i = 0; i <= products.length; i++) {
+            if (req.params.id == products[i].id) {
+                products[i].title = req.body.title;
+				products[i].author = req.body.author;
+				products[i].price = req.body.price;
+				products[i].discount = req.body.discount;
+				products[i].category = req.body.category;
+                products[i].description = req.body.description;
+                products[i].language = req.body.language;
+                products[i].format = req.body.format;
+                products[i].date = req.body.date;
+
+                break;
+            }
+        }
+		let productsJSON = JSON.stringify(products);
+        fs.writeFileSync(productsFilePath, productsJSON);
+        
+        res.redirect('/index');
+    },
+
+    destroy : function (req, res) {
+        for (let i = 0; i < products.length; i++) {
+			if (products[i].id == req.params.id) {
+				products.splice(i, 1);
+				break;
+			}
+		}
+		let productsJSON = JSON.stringify(products);
+        fs.writeFileSync(productsFilePath, productsJSON);
+        
+        res.redirect('/index');
     }
 }
 
 module.exports = productsController;
+
+// Funcion que retorna un producto (El filter no me funciona por alguna razón)
+function getProduct (id) {
+    for (let i = 0; i <= products.length; i++) {
+        if (id == products[i].id) {
+            var selectedProduct = products[i];
+            break;
+        }
+    }
+    return selectedProduct;
+}
--- bookEden/routes/products.js
+++ bookEden/routes/products.js
@@ -5,17 +5,17 @@ var router = express.Router();
 const productsController = require('../controllers/productsController');
 
 router.get('/', productsController.main); /* GET - All products */
-//router.get('/detail/:id/', productsController.detail); /* GET - Product detail */
+router.get('/detail/:id', productsController.detail); /* GET - Product detail */
 
 /*** CREATE ONE PRODUCT ***/ 
 router.get('/create/', productsController.create); /* GET - Form to create */
 //router.post('/create/', productsController.store); /* POST - Store in DB */
 
 /*** EDIT ONE PRODUCT ***/ 
-//router.get('/edit/:id', productsController.edit); /* GET - Form to create */
-//router.put('/edit/:id', productsController.update); /* PUT - Update in DB */
+router.get('/edit/:id', productsController.edit); /* GET - Form to create */
+router.put('/edit/:id', productsController.update); /* PUT - Update in DB */
 
 /*** DELETE ONE PRODUCT***/ 
-//router.delete('/delete/:id', productsController.destroy); /* DELETE - Delete from DB */
+router.delete('/delete/:id', productsController.destroy); /* DELETE - Delete from DB */
 
 module.exports = router;
