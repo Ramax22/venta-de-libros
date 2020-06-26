b4bb1d5c6aa9331018e230a8722bf52e53b00884
Lau-code <roblesma.laura@gmail.com>
2020-06-26 20:35:30 -0300

    admin

--- bookEden/database/config/config.js
+++ bookEden/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,//problemitaaaa
+    "password": "mlocura",//problemitaaaa
     "database": "bookeden",
     "host": "127.0.0.1",
     "dialect": "mysql",
