829c5c21e80831a6497617b18fc9f236da2cf4b9
Ramiro Garbagna <ramagarbagna@outlook.com>
2020-07-13 20:37:19 -0300

    agregué los libros

--- bookEden/database/config/config.js
+++ bookEden/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "bookeden_db",
+    "database": "bookeden",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": false
